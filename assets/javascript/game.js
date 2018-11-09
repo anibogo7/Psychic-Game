7193d9ee55d7f707b47f78e0c1447a0fc8580bf1
anibogo7 <a.bogosian7@gmail.com>
2018-11-08 23:37:14 -0800

    homework week 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,71 @@
-var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n', 'o','p','q','r', 's', 't','u','v','w','x','y','z']
+var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 var wins = 0;
 var losses = 0;
-var gusses = 9;
-var ettersGuessed = [];
-var gussingletters = null;
+var guesses = 9;
+var guessesRemaining = 9;
+var lettersGuessed = [];
+var guessingLetters = null;
 
 
-var computerGusses =  computerChoices[Math.floor 
-    (math.random() * computerChoices.length)];
+var computerGuesses = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    var updateGuessRemaining = function () {this .gussingletters = this. computerChoices.length)];
-        
-var updateGuessAllready = function (){
-    totalGuesses = 9; 
-    gussesRemainig = 9;
+
+
+function updateGuessesRemaining() {
+    document.querySelector('#guessRemaining').innerHTML = "Guesses remaining: " + guessesRemaining;
+};
+
+function updateGuessingLetters() {
+    this.guessingLetters = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+
+function updateGuessedAllready() {
+    document.querySelector('#letterGuess').innerHTML = "What you have guessed so far: " + lettersGuessed.join(', ');
+};
+
+var reset = function() {
+    totalGuesses = 9;
+    guessesRemaining = 9;
     lettersGuessed = [];
-     updateGuessletters();
-     updateGuessRemaining();
-     updateGuessAllready();
 
+    updateGuessingLetters();
+    updateGuessesRemaining();
+    updateGuessedAllready();
+}
 
-     updateGuessletters();
-     updateGuessRemaining();
+updateGuessingLetters();
+updateGuessesRemaining();
 
-     document.onkeyup = function (event){
-         guessRemaining--;
-         var userGuess =string.fromCharcode(event.keyCode).toLocaleLowerCase();
-         
+document.onkeyup = function(event) {
+    
+    var userGuess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
+    var check = computerChoices.includes(userGuess);
 
-         lettersGuessed.push(userGuess);
-         updateGuessRemaining();
-         updateGuessAllready();
+    if (check === false) {
+        alert("Not a valid guess. Try again.");
+    }
 
-         if (guessesRemaining> 0){
-             if(userGuess == guessingLetters){
-                 win++
-                 document.querySelector('#wins').innerHTML ="Wins:" + wins;
-                 alert("The letter you guessed was" + guessingLetter + "that was the right letter!");
-                 reset();
+    else if (check === true) {
+        guessesRemaining--;
+        lettersGuessed.push(userGuess);
+        updateGuessesRemaining();
+        updateGuessedAllready();
 
-          
-         
-     }
-     else if (gussesRemainig == 0){
-         losses ++;
-         document.querySelector('#Losses').innerHTML = "losses:" + losses;
-         alert( "The letter you were traying to guess was" + gussingletters);
-         reset();
+        if (guessesRemaining > 0){
+            if (userGuess == guessingLetters){
+                wins++
+                document.querySelector(wins).innerHTML = "Wins: " + wins;
+                alert("The letter you guessed was " + guessingLetters + "that was the right letter!");
+                reset();
+            }
+        }
 
-
-     }
-      
-} ; 
+        else if(guessesRemaining == 0){
+            losses++;
+            document.querySelector(losses).innerHTML = "Losses: " + losses;
+            alert("The letter you were trying to guess was " + guessingLetters);
+            reset();
+        }
+    }
+};    
